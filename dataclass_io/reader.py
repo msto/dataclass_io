b0d99dd60fed3cfefdd54a262833892880b1ed75
Matt Stone <matt@fulcrumgenomics.com>
2024-03-23 09:12:07 -0400

    wip: add dictreader

--- dataclass_io/reader.py
+++ dataclass_io/reader.py
@@ -1,13 +1,26 @@
-
+from csv import DictReader
 from pathlib import Path
 from types import TracebackType
+from typing import Any
 from typing import Type
 
 
 class DataclassReader:
-    def __init__(self, path: Path):
+    def __init__(
+        self,
+        path: Path,
+        dc_type: type,
+        **kwds: Any,
+    ) -> None:
+        """
+        Args:
+            path: Path to the file to read.
+            dc_type: Dataclass type.
+        """
 
-        self._reader = path.open("r")
+        self._dc_type = dc_type
+        self._fin = path.open("r")
+        self._reader = DictReader(self._fin, **kwds)
 
     def __enter__(self) -> "DataclassReader":
         return self
@@ -18,10 +31,10 @@ class DataclassReader:
         exc_value: BaseException,
         traceback: TracebackType,
     ) -> None:
-        self._reader.close()
+        self._fin.close()
 
     def __iter__(self) -> "DataclassReader":
         return self
 
-    def __next__(self) -> str:
+    def __next__(self) -> dict[str, str]:
         return next(self._reader)
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,15 +1,23 @@
-
+from dataclasses import dataclass
 from pathlib import Path
 
 from dataclass_io.reader import DataclassReader
 
 
+@dataclass
+class FakeDataclass:
+    foo: str
+    bar: int
+
+
 def test_reader(tmp_path: Path) -> None:
+    fpath = tmp_path / "test.txt"
 
-    with open(tmp_path / "test.txt", "w") as f:
-        f.write("abc\n")
+    with open(fpath, "w") as f:
+        f.write("abc\t1\n")
 
-    with DataclassReader(tmp_path / "test.txt") as reader:
-        lines = [line.strip() for line in reader]
+    dictreader_kwargs = {"fieldnames": ["foo", "bar"], "delimiter": "\t"}
+    with DataclassReader(path=fpath, dc_type=FakeDataclass, **dictreader_kwargs) as reader:
+        rows = [row for row in reader]
 
-    assert lines == ["abc"]
+    assert rows[0] == {"foo": "abc", "bar": "1"}
