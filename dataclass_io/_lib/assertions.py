1d7aa08812499e1b263c74cbe0447936111d3199
Matt Stone <matt@fulcrumgenomics.com>
2024-03-24 20:45:56 -0400

    wip: add assert_file_is_appendable()

--- dataclass_io/_lib/assertions.py
+++ dataclass_io/_lib/assertions.py
@@ -2,9 +2,12 @@ from dataclasses import is_dataclass
 from os import R_OK
 from os import W_OK
 from os import access
+from os import stat
 from pathlib import Path
 
 from dataclass_io._lib.dataclass_extensions import DataclassInstance
+from dataclass_io._lib.dataclass_extensions import fieldnames
+from dataclass_io._lib.file import get_header
 
 
 def assert_file_is_readable(path: Path) -> None:
@@ -62,6 +65,40 @@ def assert_file_is_writable(path: Path, overwrite: bool = True) -> None:
         if not access(path.parent, W_OK):
             raise PermissionError(
                 f"The specified directory for the output file path is not writable: {path.parent}"
+            )
+
+
+def assert_file_is_appendable(path: Path, dataclass_type: type[DataclassInstance]) -> None:
+    if not path.exists():
+        raise FileNotFoundError(f"The specified output file does not exist: {path}")
+
+    if not path.is_file():
+        raise IsADirectoryError(f"The specified output file path is a directory: {path}")
+
+    if not access(path, W_OK):
+        raise PermissionError(f"The specified output file is not writable: {path}")
+
+    if stat(path).st_size == 0:
+        raise ValueError(f"The specified output file is empty: {path}")
+
+    if not access(path, R_OK):
+        raise PermissionError(
+            f"The specified output file is not readable: {path}\n"
+            "The output file must be readable to append to it. "
+            "The header of the existing output file is checked for consistency with the provided "
+            "dataclass before appending to it."
+        )
+
+    # TODO: pass delimiter and header_comment_char to get_header
+    with path.open("r") as f:
+        header = get_header(f)
+        if header is None:
+            raise ValueError(f"Could not find a header in the specified output file: {path}")
+
+        if header.fieldnames != fieldnames(dataclass_type):
+            raise ValueError(
+                "The specified output file does not have the same field names as the provided "
+                f"dataclass {path}"
             )
 
 
