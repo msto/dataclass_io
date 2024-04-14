ecddea4d4fd50654a013cf2ed614c05eab3a4b4d
Matt Stone <matthew.stone12@gmail.com>
2024-04-13 21:41:11 -0400

    feat: permit str or path filename

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -13,11 +13,25 @@ class FakeDataclass:
 def test_reader(tmp_path: Path) -> None:
     fpath = tmp_path / "test.txt"
 
-    with open(fpath, "w") as f:
+    with fpath.open("w") as f:
         f.write("foo\tbar\n")
         f.write("abc\t1\n")
 
-    with DataclassReader(path=fpath, dataclass_type=FakeDataclass) as reader:
+    with DataclassReader(filename=fpath, dataclass_type=FakeDataclass) as reader:
+        rows = [row for row in reader]
+
+    assert rows[0] == FakeDataclass(foo="abc", bar=1)
+
+
+def test_reader_from_str(tmp_path: Path) -> None:
+    """Test that we can create a reader when `filename` is a `str`."""
+    fpath = tmp_path / "test.txt"
+
+    with fpath.open("w") as f:
+        f.write("foo\tbar\n")
+        f.write("abc\t1\n")
+
+    with DataclassReader(filename=str(fpath), dataclass_type=FakeDataclass) as reader:
         rows = [row for row in reader]
 
     assert rows[0] == FakeDataclass(foo="abc", bar=1)
